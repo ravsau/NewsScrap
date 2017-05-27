997f17cf1b9c92f324dd04a83a5c45f898dc42aa
rob <rob@Robs-MBP.home>
2017-05-27 16:45:20 -0400

    first commit

---
+++ scrap.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+
+
+import requests
+
+
+
+
+
+
+
+
+
+
+result=requests.get('http://www.ekantipur.com//',timeout=2)
+
+c=result.content
+
+
+
+#print(source.data)
+
+soup=BeautifulSoup(c,'html.parser')
+
+
+
+headlines=[]
+for text in soup.find_all('h1'):
+	a=text.text
+	
+
+	if len(a.split())>3:
+
+        
+		
+		headlines.append(text.text)
+
+
+for i in headlines:
+	print (i[5])
+
+
+
+
+
